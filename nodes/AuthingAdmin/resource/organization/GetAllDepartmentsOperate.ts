6e2cb8e4cb270bff735337862ddb7772e5714a6a
domi-lucky <dengxiaomei714@gmail.com>
2026-04-16 15:00:30 +0800

    feat:新增获取所有部门列表节点

---
+++ nodes/AuthingAdmin/resource/organization/GetAllDepartmentsOperate.ts
@@ -0,0 +1,102 @@
+import { IDataObject, IExecuteFunctions } from 'n8n-workflow';
+import RequestUtils from '../../../help/utils/RequestUtils';
+import { ResourceOperations } from '../../../help/type/IResource';
+
+const GetAllDepartmentsOperate: ResourceOperations = {
+    name: '获取所有部门列表',
+    value: 'getAllDepartments',
+    action: '获取所有部门列表',
+    options: [
+        {
+            displayName: '组织 Code',
+            name: 'organizationCode',
+            type: 'string',
+            required: true,
+            default: '0',
+            placeholder: '例如：steamory',
+            description: 'The unique code of the organization',
+        },
+        {
+            displayName: 'Options',
+            name: 'options',
+            type: 'collection',
+            placeholder: 'Add option',
+            default: {},
+            options: [
+                {
+                    displayName: '部门 ID',
+                    name: 'departmentId',
+                    type: 'string',
+                    default: '',
+                    placeholder: '例如：root',
+                    description: 'Department ID. Defaults to the root department ID if not specified',
+                },
+                {
+                    displayName: '部门 ID 类型',
+                    name: 'departmentIdType',
+                    type: 'options',
+                    default: 'department_id',
+                    options: [
+                        {
+                            name: 'department_id',
+                            value: 'department_id',
+                        },
+                        {
+                            name: 'open_department_id',
+                            value: 'open_department_id',
+                        },
+                    ],
+                    description: 'The type of department ID used in this request',
+                },
+                {
+                    displayName: 'With Custom Data',
+                    name: 'withCustomData',
+                    type: 'boolean',
+                    default: false,
+                    description: 'Whether to get custom data',
+                },
+                {
+                    displayName: 'With Post',
+                    name: 'withPost',
+                    type: 'boolean',
+                    default: false,
+                    description: 'Whether to get post data',
+                },
+            ],
+        },
+    ],
+    async call(this: IExecuteFunctions, index: number): Promise<IDataObject | IDataObject[]> {
+        const organizationCode = this.getNodeParameter('organizationCode', index, '') as string;
+        const options = this.getNodeParameter('options', index, {}) as IDataObject;
+
+        if (!organizationCode || organizationCode.trim() === '') {
+            throw new Error('Organization code is required and cannot be empty');
+        }
+
+        const qs: IDataObject = { organizationCode };
+
+        if (options.departmentId !== undefined && options.departmentId !== '') {
+            qs.departmentId = options.departmentId;
+        }
+        if (options.departmentIdType !== undefined) {
+            qs.departmentIdType = options.departmentIdType;
+        }
+        if (options.withCustomData !== undefined) {
+            qs.withCustomData = options.withCustomData;
+        }
+        if (options.withPost !== undefined) {
+            qs.withPost = options.withPost;
+        }
+
+        const response = await RequestUtils.request.call(this, {
+            method: 'GET',
+            url: '/api/v3/get-all-departments',
+            qs,
+        });
+
+        const data = response as any;
+        return (data?.list ?? data) as IDataObject | IDataObject[];
+    },
+};
+
+export default GetAllDepartmentsOperate;
---
+++ nodes/AuthingAdmin/resource/organizationResource.ts
@@ -0,0 +1,8 @@
+import { ResourceOptions } from '../../help/type/IResource';
+
+const OrganizationResource: ResourceOptions = {
+    name: '组织与部门管理',
+    value: 'organization',
+};
+
+export default OrganizationResource;
